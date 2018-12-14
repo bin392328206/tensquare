51e1cbe2f1037d8ac82a974d77ea83ce367ad3b3
wangbin <392328206@qq.com>
2018-12-14 23:16:34 +0800

    创建项目

---
+++ src/test/java/com/qf/ten/square/TensquareApplicationTests.java
@@ -0,0 +1,17 @@
+package com.qf.ten.square;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TensquareApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
